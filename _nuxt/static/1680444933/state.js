3179a6e497f01b0024b30e4c8f048fa389a148eb
wanLo <mail@geahnert.de>
2023-04-02 16:15:35 +0200

    Updates

---
+++ _nuxt/static/1680444933/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fvalues-to-country\u002F_nuxt\u002Fstatic\u002F1680444933",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fvalues-to-country\u002F",assetsPath:"\u002Fvalues-to-country\u002F_nuxt\u002F",cdnURL:a}}}}(null));
